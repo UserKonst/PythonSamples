bccfd44c0ff8f7ee092c0fb6fe8f06162e31a836
kkovbasa <konstantin.kovbasa@dataart.com>
2022-10-01 19:31:20 +0300

    send emails added

---
+++ emails/send_email.py
@@ -0,0 +1,20 @@
+import smtplib
+
+login = 'kostyan.vk@gmail.com'
+
+# another port 465
+smtp = smtplib.SMTP('smtp.gmail.com', 587)
+smtp.ehlo()
+smtp.starttls()
+
+password = input('enter password: ')
+smtp.login(login, password)
+
+from_address = login
+to_address = login
+subject = 'test'
+message = 'test message'
+msg = f'Subject: {subject}\n {message}'
+
+smtp.sendmail(from_address, to_address, msg)
+smtp.quit()
